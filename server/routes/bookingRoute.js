a590b37fd21882f8a1712150ae28965fcd493628
TheSoftNode <thesoftnode@gmail.com>
2024-09-21 21:16:22 +0100

    Add booking routes

---
+++ server/routes/bookingRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  getCheckoutSession,
+  getAllBookings,
+  createBooking,
+  getBooking,
+  updateBooking,
+  deleteBooking,
+} from "./../controllers/bookingController.js";
+import { isAuthenticated } from "../middlewares/protectRoutes.js";
+import restrictTo from "../middlewares/roleManager.js";
+
+const router = express.Router();
+
+router.use(isAuthenticated);
+
+router.get("/checkout-session/:tourId", getCheckoutSession);
+
+router.use(restrictTo("admin", "lead-guide"));
+
+router.route("/").get(getAllBookings).post(createBooking);
+
+router.route("/:id").get(getBooking).patch(updateBooking).delete(deleteBooking);
+
+export default router;
