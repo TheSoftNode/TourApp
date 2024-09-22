e96334999cfbd6b603492ed3cfcd68962bf1896d
TheSoftNode <thesoftnode@gmail.com>
2024-09-22 23:44:43 +0100

    created the router component

--- client/src/Routes/Router.jsx
+++ client/src/Routes/Router.jsx
@@ -1,13 +1,13 @@
-import Home from '../Pages/Home'
 import Login from '../Pages/Login'
-import Signup from '../Pages/Signup'
+import SignUp from '../pages/SignUp'
 import Contact from '../Pages/Contact'
 import Verification from '../Pages/Verification';
-import MyAccount from '../Dashboard/user-account/MyAccount'
+import ToursList from '../pages/ToursList';
+import TourDetails from '../pages/TourDetails';
+
 
 
 import {Routes, Route} from 'react-router-dom';
-import ProtectedRoute from './ProtectedRoute'
 import CheckoutSuccess from '../Pages/Doctors/CheckoutSucess'
 import CheckoutFail from '../Pages/Doctors/CheckoutFail'
 import ForgotPassword from '@/Pages/ForgotPassword'
@@ -16,43 +16,18 @@ import ResetPassword from '@/Pages/ResetPassword'
 
 const Routers = () => {
   return <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/doctors" element={<Doctors/>} />
-        <Route path="/doctors/:id" element={<DoctorDetail/>} />
+        <Route path="/" element={<ToursList/>} />
+        <Route path="/home" element={<ToursList/>} />
+        <Route path="/tours" element={<ToursList/>} />
+        <Route path="/tours/:id" element={<TourDetails/>} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Signup/>} />
+        <Route path="/signup" element={<SignUp/>} />
         <Route path="/verify" element={<Verification/>} />
         <Route path="/contact" element={<Contact/>} />
-        <Route path="/services" element={<Services/>} />
         <Route path="/forgot-password" element={<ForgotPassword/>} />
         <Route path="/reset-password" element={<ResetPassword/>} />
         <Route path="/checkout-success" element={<CheckoutSuccess />} />
         <Route path="/checkout-fail" element={<CheckoutFail />} />
-        <Route 
-        path="/users/profile/me" 
-        element={
-            <ProtectedRoute allowedRoles={["patient"]}>
-                <MyAccount/>
-            </ProtectedRoute>
-        } 
-        />
-        <Route 
-            path="/doctors/profile/me" 
-            element={
-                <ProtectedRoute allowedRoles={["doctor"]}>
-                    <Dashboard/>
-                </ProtectedRoute>
-            } 
-        />
-        <Route 
-            path="/admin/dashboard"
-            element={
-                <ProtectedRoute allowedRoles={["admin"]}>
-                    <AdminDashboard />
-                </ProtectedRoute>
-            }
-        />
         
 
     </Routes>
