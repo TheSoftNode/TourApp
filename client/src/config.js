6b5f9a4eb32f2a35ec47426ef3f099cfa9fd1df0
TheSoftNode <thesoftnode@gmail.com>
2024-09-25 18:48:20 +0100

    Add the backend url

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://docmeet-web-app-2.onrender.com/api/v1";
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://natours-mhtj.onrender.com/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";
 export const token = localStorage.getItem("token");
